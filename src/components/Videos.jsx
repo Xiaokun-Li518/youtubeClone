13f5d7df4618144de9092077cc2781504e4ee884
li,xiaokun <leeli@iu.edu>
2023-03-06 16:39:30 -0500

    fix channel card sizing

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,8 +7,8 @@ const Videos = ({ videos, direction }) => (
     <Stack direction= { direction || "row"} flexWrap="wrap" justifyContent="center" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
-          {item?.id?.videoId && <VideoCard video={item} /> }
-          {item?.id.channelId && <ChannelCard channelDetail={item} />}
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
